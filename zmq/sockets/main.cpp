af5291588ffda9659ba143c065f5c40efc12cc87
Robert Albus <hello@robertalbus.com>
2023-12-08 23:30:29 -0700

    Convert pthread to jthread in ZmqSockets experiment

--- zmq/sockets/main.cpp
+++ zmq/sockets/main.cpp
@@ -1,54 +1,8 @@
-//
-//  Multithreaded relay in C++
-//
+#include <thread>
+
 
 #include "./../zhelpers.hpp"
 
-//  Step 1 pushes one message to step 2
-
-void *step1 (void *arg) {
-	
-	zmq::context_t * context = static_cast<zmq::context_t*>(arg);
-	
-	//  Signal downstream to step 2
-	zmq::socket_t sender (*context, ZMQ_PUSH);
-	sender.connect("inproc://step2");
-
-    zmq::message_t message(0);
-    sender.send (message, 0);
-
-	return (NULL);
-}
-
-//  Step 2 relays the signal to step 3
-
-void *step2 (void *arg) {
-
-	zmq::context_t * context = static_cast<zmq::context_t*>(arg);
-	
-    //  Bind to inproc: endpoint, then start upstream thread
-	zmq::socket_t receiver (*context, ZMQ_PULL);
-    receiver.bind("inproc://step2");
-
-    pthread_t thread;
-    pthread_create (&thread, NULL, step1, context);
-
-    //  Wait for signal
-    zmq::message_t messageReceive;
-    receiver.recv(&messageReceive, 0);
-
-    //  Signal downstream to step 3
-    zmq::socket_t sender (*context, ZMQ_PUSH);
-    sender.connect("inproc://step3");
-    s_send (sender, std::string(""));
-
-    zmq::message_t messageSend(0);
-    sender.send (messageSend, 0);
-
-    return (NULL);
-}
-
-//  Main program starts steps 1 and 2 and acts as step 3
 
 int main () {
 	
@@ -58,8 +12,35 @@ int main () {
     zmq::socket_t receiver (context, ZMQ_PULL);
     receiver.bind("inproc://step3");
 
-    pthread_t thread;
-    pthread_create (&thread, NULL, step2, &context);
+    std::jthread([&context]() {
+        zmq::socket_t receiver (context, ZMQ_PULL);
+        receiver.bind("inproc://step2");
+
+        std::jthread([&context]() {
+            //  Signal downstream to step 2
+            zmq::socket_t sender (context, ZMQ_PUSH);
+            sender.connect("inproc://step2");
+
+            zmq::message_t message(0);
+            sender.send (message, 0);
+
+            return (NULL);
+        });
+
+        //  Wait for signal
+        zmq::message_t messageReceive;
+        receiver.recv(&messageReceive, 0);
+
+        //  Signal downstream to step 3
+        zmq::socket_t sender (context, ZMQ_PUSH);
+        sender.connect("inproc://step3");
+        s_send (sender, std::string(""));
+
+        zmq::message_t messageSend(0);
+        sender.send (messageSend, 0);
+
+        return (NULL);
+    });
 
     //  Wait for signal
     // see ./../zmq/zhelpers.hpp:102 for getting the message string
