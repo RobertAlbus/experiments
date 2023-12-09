24a20f937765bb30a338c0c355b5ff089943deff
Robert Albus <hello@robertalbus.com>
2023-12-09 00:53:27 -0700

    Convert magic strings to consts

--- zmq/sockets/main.cpp
+++ zmq/sockets/main.cpp
@@ -1,7 +1,11 @@
 #include <thread>
 
-
 #include "./../zhelpers.hpp"
+
+struct PORT {
+    static constexpr const char* A = "inproc://step2";
+    static constexpr const char* B = "inproc://step3";
+};
 
 
 int main () {
@@ -10,16 +14,16 @@ int main () {
 
     //  Bind to inproc: endpoint, then start upstream thread
     zmq::socket_t receiver (context, ZMQ_PULL);
-    receiver.bind("inproc://step3");
+    receiver.bind(PORT::B);
 
     std::jthread([&context]() {
         zmq::socket_t receiver (context, ZMQ_PULL);
-        receiver.bind("inproc://step2");
+        receiver.bind(PORT::A);
 
         std::jthread([&context]() {
             //  Signal downstream to step 2
             zmq::socket_t sender (context, ZMQ_PUSH);
-            sender.connect("inproc://step2");
+            sender.connect(PORT::A);
 
             zmq::message_t message(0);
             sender.send (message, 0);
@@ -33,8 +37,9 @@ int main () {
 
         //  Signal downstream to step 3
         zmq::socket_t sender (context, ZMQ_PUSH);
-        sender.connect("inproc://step3");
-        s_send (sender, std::string(""));
+        sender.connect(PORT::B);
+        zmq::message_t message(0);
+        sender.send(message);
 
         zmq::message_t messageSend(0);
         sender.send (messageSend, 0);
