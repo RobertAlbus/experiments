30d84d09d1a38a3d55916f954db2d43c2b86ad5e
Robert Albus <hello@robertalbus.com>
2023-12-23 01:06:05 -0700

    Update LPF Coeff experiment to compare standard secon order IIR coeff generation speed with AVX2 SIMD lerp implementation of coefficients from an upper and lower bound

--- assembly/LpfCoeffs/main.cpp
+++ assembly/LpfCoeffs/main.cpp
@@ -5,7 +5,21 @@
 #include <vector>
 #include <iostream>
 
-std::tuple<float, float, float, float, float> calculateBiquadCoeffs(float cutoff, float fs, float Q) {
+#include <immintrin.h> // avx2 simd
+
+/*
+
+non-optimized: AVX2  6.54% faster
+    optimized: AVX2 36.78% faster
+
+    ????
+
+if lerping coefficients sounds fine and is accurate, then this is an excellent optimization
+
+*/
+
+std::array<float, 5> calculateBiquadCoeffs(float cutoff, float fs, float Q) {
+// std::tuple<float, float, float, float, float> calculateBiquadCoeffs(float cutoff, float fs, float Q) {
     const float omega = 2.0 * M_PI * cutoff / fs;
     const float alpha = std::sin(omega) / (2.0 * Q);
     const float cos_omega = std::cos(omega);
@@ -26,8 +40,41 @@ std::tuple<float, float, float, float, float> calculateBiquadCoeffs(float cutoff
     return {b0, b1, b2, a1, a2};
 }
 
+std::array<float, 5> lerpBiquadCoeffs(
+    std::array<float, 5>& start,
+    std::array<float, 5>& end,
+    float amount
+) {
+
+    __m256 vec_start = _mm256_set_ps(
+        start[0],
+        start[1],
+        start[2],
+        start[3],
+        start[4],
+        0,0,0 // only need 5 slots
+    );
+    __m256 vec_end = _mm256_set_ps(
+        end[0],
+        end[1],
+        end[2],
+        end[3],
+        end[4],
+        0,0,0 // only need 5 slots
+    );
+    __m256 t = _mm256_set1_ps(amount);
+
+    // Perform SIMD lerp for 8 data points
+    __m256 result = _mm256_add_ps(vec_start, _mm256_mul_ps(t, _mm256_sub_ps(vec_end, vec_start)));
+
+    float output[8];
+    _mm256_storeu_ps(output, result);
+
+    return {output[0], output[1], output[2], output[3], output[4]};
+}
+
 int main() {
-    printf("\n111");
+    printf("\n----------------------------------------------------------------");
     const int durationSamples = 100000;
     const int numFilters = 800;
     const int iterations = durationSamples * numFilters;
@@ -36,27 +83,47 @@ int main() {
     float cutoff = 3000;
     float Q = 1.414;
 
-    // const size_t iterations_size_t = static_cast<size_t>(iterations);
-
+    std::vector<std::array<float, 5>> coeffs;
     // std::vector<std::tuple<float, float, float, float, float>> coeffs;
-    std::array<std::tuple<float, float, float, float, float>, 80000000> coeffs;
-    // coeffs.reserve((size_t)iterations); // reserving the right amount of memory has a huge impact on performance.
+    coeffs.reserve((size_t)iterations); // reserving the right amount of memory has a huge impact on performance.
     
     auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < iterations; ++i) {
-        printf("\n%i",i);
-        coeffs[i] = calculateBiquadCoeffs(cutoff + (float)i, sampleRate, Q);
-        // coeffs.emplace_back(calculateBiquadCoeffs(cutoff + (float)i, fs, Q));
+        coeffs.emplace_back(calculateBiquadCoeffs(cutoff + (float)i, sampleRate, Q));
     }
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-    printf("\n%i paralell biquad lpf filters | duration %i ms of %i ms audio | %04.2f%% time budget consumed",
+    float freqStart = 20.f;
+    float freqEnd = 20000.f;
+    float freqRange = freqEnd - freqStart;
+    float freqRangeRecip = freqRange / 1;
+    float cutoffAdjusted = cutoff - freqStart;
+    float cutoffCompensation = freqStart * freqRangeRecip; // compensate for bottom freq not being 0
+    auto startCoeffs = calculateBiquadCoeffs(freqStart, sampleRate, Q);
+    auto endCoeffs = calculateBiquadCoeffs(freqEnd, sampleRate, Q);
+
+    auto simd_start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < iterations; ++i) {
+        float lerpAmount = (cutoffAdjusted + i) * freqRangeRecip + cutoffCompensation;
+        coeffs.emplace_back(lerpBiquadCoeffs(startCoeffs, endCoeffs, lerpAmount));
+    }
+    auto simd_end = std::chrono::high_resolution_clock::now();
+    auto simd_duration = std::chrono::duration_cast<std::chrono::milliseconds>(simd_end - simd_start).count();
+
+    printf("\n%i parallel biquad lpf filters | duration %i ms of %i ms audio | %04.2f%% time budget consumed [COEFF CALCULATION]",
         numFilters,
         duration,
         (int)audioDurationMs,
         ((float)duration) / audioDurationMs * 100.f
     );
+    printf("\n%i parallel biquad lpf filters | duration %i ms of %i ms audio | %04.2f%% time budget consumed [AVX2 SIMD LERP]",
+        numFilters,
+        simd_duration,
+        (int)audioDurationMs,
+        ((float)simd_duration) / audioDurationMs * 100.f
+    );
+    printf("\n");
 
     return 0;
 }
