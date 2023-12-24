2deb756f84b0fd05db5bd7823ede28c7d8bafbee
Robert Albus <hello@robertalbus.com>
2023-12-24 16:15:31 -0700

    Plot the change in coefficients (per coefficient) for 1hz - 24khz for 5 different Q values

--- python/coeff_plotting/plot_coeffs_by_position.py
+++ python/coeff_plotting/plot_coeffs_by_position.py
@@ -4,47 +4,48 @@ import matplotlib.pyplot as plt
 from lib.generate_coeffs import *
 
 sample_rate = 48000
-Q = 0.707
 gain_db = -60
 
+Qs = [0.15, 0.707, 1.414, 2.828, 5.656]
 
 filter_coeffs_linear = {}
 for freq in range(1, 24001):
-    a, b = biquad_resonant_lp_coeffs(freq, Q, gain_db, sample_rate)
-    filter_coeffs_linear[str(freq)] = Coeffs(a, b)
+    filter_coeffs_linear[str(freq)] = {}
+    for Q in Qs:
+        a, b = biquad_resonant_lp_coeffs(freq, Q, gain_db, sample_rate)
+        filter_coeffs_linear[str(freq)][str(Q)] = Coeffs(a, b)
+coeffs = {}
+for Q in Qs:
+    coeffs[str(Q)] = {
+        "a1": [],
+        "a2": [],
+        "b0": [],
+        "b1": [],
+        "b2": [],
+    }
 
-coeffs = {
-    "a1": [],
-    "a2": [],
-    "b0": [],
-    "b1": [],
-    "b2": [],
-}
 for freq_key, coefficient_set in filter_coeffs_linear.items():
-    coeffs["a1"].append(coefficient_set.a[0])
-    coeffs["a2"].append(coefficient_set.a[1])
-    coeffs["b0"].append(coefficient_set.b[0])
-    coeffs["b1"].append(coefficient_set.b[1])
-    coeffs["b2"].append(coefficient_set.b[2])
+    for Q, coefficients in coefficient_set.items():
+        coeffs[Q]["a1"].append(coefficients.a[0])
+        coeffs[Q]["a2"].append(coefficients.a[1])
+        coeffs[Q]["b0"].append(coefficients.b[0])
+        coeffs[Q]["b1"].append(coefficients.b[1])
+        coeffs[Q]["b2"].append(coefficients.b[2])
 
 
-fig, axs = plt.subplots(1,5)
+fig, axs = plt.subplots(len(Qs), 5)
 
-
-axs[0].plot(list(map(lambda x: -3 + ((x/24000) * 3), range(1, 24001))), coeffs["a1"])
-axs[0].set_title("a1")
-
-axs[1].plot(list(map(lambda x: ((x/24000) * 2), range(1, 24001))), coeffs["a2"])
-axs[1].set_title("a2")
-
-axs[2].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs["b0"])
-axs[2].set_title("b0")
-
-axs[3].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs["b1"])
-axs[3].set_title("b1")
-
-axs[4].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs["b2"])
-axs[4].set_title("b2")
+for i, Q in enumerate(Qs):
+    axs[i, 0].plot(list(map(lambda x: -3 + ((x/24000) * 3), range(1, 24001))), coeffs[str(Q)]["a1"])
+    axs[i, 0].set_title("a1")
+    axs[i, 1].plot(list(map(lambda x: ((x/24000) * 2), range(1, 24001))), coeffs[str(Q)]["a2"])
+    axs[i, 1].set_title("a2")
+    axs[i, 2].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b0"])
+    axs[i, 2].set_title("b0")
+    axs[i, 3].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b1"])
+    axs[i, 3].set_title("b1")
+    axs[i, 4].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b2"])
+    axs[i, 4].set_title("b2")
 
 
 # a_coeff_plot_lin.legend()
