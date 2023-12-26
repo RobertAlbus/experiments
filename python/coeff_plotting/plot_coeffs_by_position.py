c4691fbd20372c18115edb9b8398232a45d0a065
Robert Albus <hello@robertalbus.com>
2023-12-26 00:35:00 -0700

    Refactor python coefficient plotting

--- python/coeff_plotting/plot_coeffs_by_position.py
+++ python/coeff_plotting/plot_coeffs_by_position.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 
 from lib.generate_coeffs import *
 
-sample_rate = 48000
-gain_db = -60
 
+filter_func = biquad_resonant_bp_coeffs
+
+sample_rate = 96000
 Qs = [0.15, 0.707, 1.414, 2.828, 5.656]
 
+cutoff_freq_start = 20
+cutoff_freq_end = 24000
+cutoff_freq_range = cutoff_freq_end - cutoff_freq_start
+
+
 filter_coeffs_linear = {}
-for freq in range(1, 24001):
+for freq in range(cutoff_freq_start, cutoff_freq_end):
     filter_coeffs_linear[str(freq)] = {}
     for Q in Qs:
-        a, b = biquad_resonant_lp_coeffs(freq, Q, gain_db, sample_rate)
+        a, b = filter_func(freq, Q, sample_rate)
         filter_coeffs_linear[str(freq)][str(Q)] = Coeffs(a, b)
 coeffs = {}
 for Q in Qs:
@@ -36,15 +42,39 @@ for freq_key, coefficient_set in filter_coeffs_linear.items():
 fig, axs = plt.subplots(len(Qs), 5)
 
 for i, Q in enumerate(Qs):
-    axs[i, 0].plot(list(map(lambda x: -3 + ((x/24000) * 3), range(1, 24001))), coeffs[str(Q)]["a1"])
+    axs[i, 0].plot(
+        list(map(lambda x: ((x/cutoff_freq_range)),
+            range(0, cutoff_freq_range))),
+        coeffs[str(Q)]["a1"]
+    )
     axs[i, 0].set_title("a1")
-    axs[i, 1].plot(list(map(lambda x: ((x/24000) * 2), range(1, 24001))), coeffs[str(Q)]["a2"])
+
+    axs[i, 1].plot(
+        list(map(lambda x: (x/cutoff_freq_range),
+            range(0, cutoff_freq_range))),
+        coeffs[str(Q)]["a2"]
+    )
     axs[i, 1].set_title("a2")
-    axs[i, 2].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b0"])
+
+    axs[i, 2].plot(
+        list(map(lambda x: (x/cutoff_freq_range),
+            range(0, cutoff_freq_range))),
+        coeffs[str(Q)]["b0"]
+    )
     axs[i, 2].set_title("b0")
-    axs[i, 3].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b1"])
+
+    axs[i, 3].plot(
+        list(map(lambda x: (x/cutoff_freq_range),
+            range(0, cutoff_freq_range))),
+        coeffs[str(Q)]["b1"]
+    )
     axs[i, 3].set_title("b1")
-    axs[i, 4].plot(list(map(lambda x: (x/24000), range(1, 24001))), coeffs[str(Q)]["b2"])
+    
+    axs[i, 4].plot(
+        list(map(lambda x: (x/cutoff_freq_range),
+            range(0, cutoff_freq_range))),
+        coeffs[str(Q)]["b2"]
+    )
     axs[i, 4].set_title("b2")
 
 
